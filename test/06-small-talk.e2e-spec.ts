9fcb6fbea3710b2fdb2857c00b4a56083cb93672
gilpop8663 <wolfye@naver.com>
2024-07-11 13:57:02 +0900

    test: 스몰톡 수정하는 기능 테스트 코드 작성

--- test/06-small-talk.e2e-spec.ts
+++ test/06-small-talk.e2e-spec.ts
@@ -55,3 +55,64 @@ test('스몰톡을 생성한다.', async () => {
   expect(smallTalk.authorComment).toBe('ㅇㅇ');
   expect(smallTalk.point).toBe(300);
 });
+
+test('스몰톡을 수정한다.', async () => {
+  const [initialSmallTalk] = await smallTalkRepository.find();
+
+  const EDITED = {
+    title: '수정1',
+    content: '수정2',
+    authorComment: '수정3',
+    thumbnailUrl: '수정4',
+    point: 0,
+  };
+
+  expect(initialSmallTalk.title).not.toBe(EDITED.title);
+  expect(initialSmallTalk.content).not.toBe(EDITED.content);
+  expect(initialSmallTalk.authorComment).not.toBe(EDITED.authorComment);
+  expect(initialSmallTalk.point).not.toBe(EDITED.point);
+  expect(initialSmallTalk.thumbnailUrl).not.toBe(EDITED.thumbnailUrl);
+
+  await request(app.getHttpServer())
+    .post(GRAPHQL_ENDPOINT)
+    .send({
+      query: /* GraphQL */ `
+        mutation {
+          editSmallTalk(
+            input: {
+              smallTalkId: ${initialSmallTalk.id}
+              title: "${EDITED.title}"
+              content: "${EDITED.content}"
+              authorComment: "${EDITED.authorComment}"
+              thumbnailUrl: "${EDITED.thumbnailUrl}"
+              point: ${EDITED.point}
+            }
+          ) {
+            ok
+            error
+          }
+        }
+      `,
+    })
+    .expect(200)
+    .expect((res) => {
+      const {
+        body: {
+          data: { editSmallTalk },
+        },
+      } = res;
+
+      expect(editSmallTalk.ok).toBe(true);
+      expect(editSmallTalk.error).toBe(null);
+    });
+
+  const updatedSmallTalk = await smallTalkRepository.findOne({
+    where: { id: initialSmallTalk.id },
+  });
+
+  expect(updatedSmallTalk.title).toBe(EDITED.title);
+  expect(updatedSmallTalk.content).toBe(EDITED.content);
+  expect(updatedSmallTalk.authorComment).toBe(EDITED.authorComment);
+  expect(updatedSmallTalk.point).toBe(EDITED.point);
+  expect(updatedSmallTalk.thumbnailUrl).toBe(EDITED.thumbnailUrl);
+});
